46d5251b05c2d36d72f5943eda85efa5d8b89708
cwf666 <m17632819213@163.com>
2019-01-23 11:13:03 +0800

    movie

---
+++ app/src/main/java/com/bw/movie/utils/RetroUtils.java
@@ -0,0 +1,33 @@
+package com.bw.movie.utils;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * author:author${车文飞}
+ * data:2019/1/23
+ */
+public class RetroUtils {
+    private RetroUtils() {
+        HttpLoggingInterceptor httpLoggingInterceptor=new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient okHttpClient=new OkHttpClient.Builder()
+                .writeTimeout(20,TimeUnit.SECONDS)
+                .readTimeout(20,TimeUnit.SECONDS)
+                .connectTimeout(20,TimeUnit.SECONDS)
+                .addInterceptor(httpLoggingInterceptor)
+                .build();
+        Retrofit retrofit=new Retrofit.Builder()
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("")
+                .client(okHttpClient)
+                .build();
+
+    }
+}
