4d401f59982b062f445fb56b8b4508b9d0f57a02
chewenfei <17632819213@163.com>
2019-01-26 18:48:47 +0800

    .....

--- app/src/main/java/com/bw/movie/fragment/MyFragment.java
+++ app/src/main/java/com/bw/movie/fragment/MyFragment.java
@@ -14,6 +14,7 @@ import com.bw.movie.base.BaseFragment;
  * A simple {@link Fragment} subclass.
  */
 public class MyFragment extends BaseFragment {
+    //nii,,,
 
     @Override
     protected int setLayoutId() {
@@ -23,8 +24,6 @@ public class MyFragment extends BaseFragment {
 
     @Override
     protected void init(View view, Bundle savedInstanceState) {
-
-
 
     }
 
